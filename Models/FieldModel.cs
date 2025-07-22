574239c394dff2f31eab6637d5409c0329c271a1
GreenUserInq <and.red.alpin@gmail.com>
2025-07-22 04:37:58 +0300

    Initial commit

---
+++ Models/FieldModel.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+
+namespace AgroFieldApi.Models
+{
+    /// <summary>
+    /// Одно сельскохозяйственное поле
+    /// </summary>
+    public class FieldModel
+    {
+        private object _locations;
+        /// <summary>
+        /// Уникальный эдентификатор поля
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// Название поля
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Площадь поля в кв м
+        /// </summary>
+        public double Size { get; set; }
+        /// <summary>
+        /// Географический центр поля
+        /// </summary>
+        public GeoPoint Center { get; private set; }
+        /// <summary>
+        /// Контур поля по списку координат
+        /// </summary>
+        public List<GeoPoint> Polygon {  get; private set; }
+        /// <summary>
+        /// Структура координат: центр и полигон возвращает анонимный объект
+        /// Кэшируется при первом обращении
+        /// </summary>
+        public object Location
+        {
+            get
+            {
+                return _locations ??= new
+                {
+                    Center = new[] {Center.Lat, Center.Lng},
+                    Polygon = Polygon.Select(o=> new[] {o.Lat, o.Lng})
+                };
+            }
+        }
+
+        /// <summary>
+        /// Конструктор модели поля.
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор поля.</param>
+        /// <param name="name">Название поля.</param>
+        /// <param name="center">Центр поля.</param>
+        /// <param name="polygon">Контур поля.</param>
+        /// <param name="size">Площадь (по умолчанию 0.0).</param>
+        public FieldModel(string id, string name, GeoPoint center, List<GeoPoint> polygon, double size = 0.0) 
+        { 
+            Id = id;
+            Name = name;
+            Size = size;
+            Center = center;
+            Polygon = polygon;
+        }
+
+        /// <summary>
+        /// Обновляет центр и контур поля, сбрасывает кэш
+        /// </summary>
+        /// <param name="center"></param>
+        /// <param name="polygon"></param>
+        public void UpdateGeometry(GeoPoint center, List<GeoPoint> polygon)
+        {
+            Center = center;
+            Polygon = polygon;
+            _locations = null;
+        }
+        /// <summary>
+        /// Обновляет центр, сбрасывает кэш
+        /// </summary>
+        /// <param name="center"></param>
+        public void UpdateCenter(GeoPoint center)
+        {
+            Center = center;
+            _locations = null;
+        }
+        /// <summary>
+        /// Обновляет контур, сьрасывает кэш
+        /// </summary>
+        /// <param name="polygon"></param>
+        public void UpdatePolygon(List<GeoPoint> polygon)
+        {
+            Polygon = polygon;
+            _locations = null;
+        }
+    }
+}
---
+++ Models/GeoPoint.cs
@@ -0,0 +1,34 @@
+﻿namespace AgroFieldApi.Models
+{
+    /// <summary>
+    /// Представляет географическую точку с широтой и долготой.
+    /// </summary>
+    public class GeoPoint
+    {
+        /// <summary>
+        /// Широта (latitude).
+        /// </summary>
+        public double Lat { get; set; }
+
+        /// <summary>
+        /// Долгота (longitude).
+        /// </summary>
+        public double Lng { get; set; }
+
+        /// <summary>
+        /// Конструктор точки.
+        /// </summary>
+        /// <param name="lat">Широта</param>
+        /// <param name="lng">Долгота</param>
+        public GeoPoint(double lat, double lng)
+        {
+            Lat = lat;
+            Lng = lng;
+        }
+
+        /// <summary>
+        /// Удобное строковое представление для отладки.
+        /// </summary>
+        public override string ToString() => $"({Lat}, {Lng})";
+    }
+}
